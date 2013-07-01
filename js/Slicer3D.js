7146bcce415b49c2fd8af5f1bba48e05c8f827e2
Manny Tan <mannytan@uncontrol.com>
2013-07-01 19:53:43 -0400

    reconfigured polygon plane to match paper prototype

--- js/Slicer3D.js
+++ js/Slicer3D.js
@@ -27,12 +27,12 @@ SLICER.Slicer3D = function(name) {
 
 	this.count = 0;
 
-	// vars specific to plane
-	this.total = 0;
-	this.totalVInc = 10;
-	this.totalHInc = 10;
-	this.points = null;
+	// vars specific to slicer3D
+	this.totalVSegments = 5;
+	this.totalHSegments = 11; // must be odd
 
+	this.cylinder = null;
+	this.customPlane = null;
 
 	this.init = function() {
 		this.traceFunction("init");
@@ -95,6 +95,8 @@ SLICER.Slicer3D = function(name) {
 		var width = 600;
 		var height = 600;
 		var depth = 600;
+		var geometry, 
+			material;
 		//  -------------------------------------
 		//  draw cube
 		//  -------------------------------------
@@ -130,54 +132,24 @@ SLICER.Slicer3D = function(name) {
 
 	};
 
-function generateTexture() {
-
-		var canvas = document.createElement( 'canvas' );
-		canvas.width = 256;
-		canvas.height = 256;
-
-		var context = canvas.getContext( '2d' );
-		var image = context.getImageData( 0, 0, 256, 256 );
-
-		var x = 0, y = 0;
-
-		for ( var i = 0, j = 0, l = image.data.length; i < l; i += 4, j ++ ) {
-
-			x = j % 256;
-			y = x == 0 ? y + 1 : y;
-
-			image.data[ i ] = 255;
-			image.data[ i + 1 ] = 255;
-			image.data[ i + 2 ] = 255;
-			image.data[ i + 3 ] = Math.floor( x ^ y );
-
-		}
-
-		context.putImageData( image, 0, 0 );
-
-		return canvas;
-
-	}
 
 	this.createForegroundElements = function() {
 
-
-
 		var i,j,id,
 			particle,
-			faceNormal;
-		var totalVSegments = 11;
-		var totalHSegments = 31;
-
+			faceNormal, 
+			geometry, 
+			material;
+/*
 		// backbone dots
 		material = new THREE.ParticleBasicMaterial( { color: 0x660066, size: 3} );
 		geometry = new THREE.Geometry();
-		for(i = 0; i < totalVSegments; i++) {
+		for(i = 0; i < this.totalVSegments; i++) {
 			geometry.vertices.push(new THREE.Vector3());
 		}
-		for(i = 0; i < totalVSegments; i++) {
+		for(i = 0; i < this.totalVSegments; i++) {
 			geometry.vertices[i].x = 0;
-			geometry.vertices[i].y = i/totalVSegments * 200 - 100;
+			geometry.vertices[i].y = i/this.totalVSegments * 200 - 100;
 			geometry.vertices[i].z = 0;
 		}
 		particles = new THREE.ParticleSystem( geometry, material );
@@ -185,7 +157,7 @@ function generateTexture() {
 
 		// backbone line
 		geometry = new THREE.Geometry();
-		for(i = 0; i < totalVSegments; i++) {
+		for(i = 0; i < this.totalVSegments; i++) {
 			geometry.vertices.push(particles.geometry.vertices[i].clone());
 		}
 		material = new THREE.LineBasicMaterial({ color: 0x660000, transparent:true, opacity: 0.25 });
@@ -194,15 +166,176 @@ function generateTexture() {
 
 		var tri = new THREE.Mesh( geometry, material);
 		this.base.add(tri);
+*/
+		// this.totalHSegments accounts for duplicate wrap around
+		geometry = new THREE.CylinderGeometry( 40, 40, 100, this.totalHSegments-1, this.totalVSegments-1, true);
+		material = new THREE.MeshLambertMaterial( { color: 0x996633, shading:THREE.SmoothShading, wireframe:false} );
+		material.side = THREE.DoubleSide;
+		this.cylinder = new THREE.Mesh( geometry, material );
+		// this.base.add(this.cylinder);
 
-		// totalHSegments accounts for duplicate wrap around
-		geometry02 = new THREE.CylinderGeometry( 40, 40, 100, totalHSegments-1, totalVSegments-1, true);
-		material02 = new THREE.MeshLambertMaterial( { color: 0x996633, shading:THREE.FlatShading, wireframe:false,transparent:false,opacity:.25} );
-		material02.side = THREE.DoubleSide;
-		cylinder02 = new THREE.Mesh( geometry02, material02 );
-		this.base.add(cylinder02);
+		// custom cylinder
 
+		material = new THREE.MeshBasicMaterial( { color: 0x6699FF, wireframe:true,transparent:false,opacity:.25} );
+		
+		geometry = new THREE.Geometry()
+		geometry.vertices.push( new THREE.Vector3( -50,  50, 0 ) );
+		geometry.vertices.push( new THREE.Vector3( -50, -50, 0 ) );
+		geometry.vertices.push( new THREE.Vector3(  50, -50, 0 ) );
+		geometry.vertices.push( new THREE.Vector3(  50, 50, 0 ) );
+		geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
+		geometry.faces.push( new THREE.Face3( 0, 2, 3 ) );
+		// geometry.computeBoundingSphere();
+		// customPlane = new THREE.Mesh( geometry, material );
+		// this.base.add(customPlane);
+
+
+		// strip order 1
+		// 0 - 3 - 6
+		// | \ | / |
+		// 1 - 4 - 7
+		// | / | \ |
+		// 2 - 5 - 8
+
+		// strip order 2
+		// 0 - 3 - 6
+		// | / | \ |
+		// 1 - 4 - 7
+		// | \ | / |
+		// 2 - 5 - 8
+
+		// 1 plane = 3 + 6
+		// 2 plane = 3 + 6 + 6
+		// 3 plane = 3 + 6 + 6 + 6
+
+		this.getCustomGeometry = function(totalPlanesH, orderId, offset){
+			var geometry = new THREE.Geometry();
+			var id = 0;
+			var percentage = 0;
+			var i,j;
+			var totalVertices = 3+(6)*totalPlanesH;
+			var incV = 3;
+			var incH = 1 + (2)*totalPlanesH;
+
+			for(i = 0; i < totalVertices; i++) {
+				geometry.vertices.push(new THREE.Vector3());
+			}
+
+			for(j = 0; j < incH; j++) {
+				for(i = 0; i < incV; i++) {
+					percentage = j/(incH-1)*TWO_PI;
+					geometry.vertices[id].x = Math.sin(percentage)*100;
+					geometry.vertices[id].y = i*10 + offset;
+					geometry.vertices[id].z = Math.cos(percentage)*100;
+					id++;
+				}
+			}
+
+			if(orderId%2===0){
+				for(j = 0; j < totalPlanesH; j++) {
+					id = j*6;
+					geometry.faces.push( new THREE.Face3( id+3, id+6, id+7 ) );
+					geometry.faces.push( new THREE.Face3( id+3, id+7, id+4 ) );
+					geometry.faces.push( new THREE.Face3( id+3, id+4, id+1 ) );
+					geometry.faces.push( new THREE.Face3( id+3, id+1, id+0 ) );
+					geometry.faces.push( new THREE.Face3( id+5, id+2, id+1 ) );
+					geometry.faces.push( new THREE.Face3( id+5, id+1, id+4 ) );
+					geometry.faces.push( new THREE.Face3( id+5, id+4, id+7 ) );
+					geometry.faces.push( new THREE.Face3( id+5, id+7, id+8 ) );
+				}
+			} else {
+				for(j = 0; j < totalPlanesH; j++) {
+					id = j*6;
+					geometry.faces.push( new THREE.Face3( id+4, id+1, id+0 ) );
+					geometry.faces.push( new THREE.Face3( id+4, id+0, id+3 ) );
+					geometry.faces.push( new THREE.Face3( id+4, id+3, id+6 ) );
+					geometry.faces.push( new THREE.Face3( id+4, id+6, id+7 ) );
+
+					geometry.faces.push( new THREE.Face3( id+4, id+7, id+8 ) );
+					geometry.faces.push( new THREE.Face3( id+4, id+8, id+5 ) );
+					geometry.faces.push( new THREE.Face3( id+4, id+5, id+2 ) );
+					geometry.faces.push( new THREE.Face3( id+4, id+2, id+1 ) );
+				}
+			}
+
+			// geometry.computeBoundingSphere();
+			geometry.computeFaceNormals(); 
+			geometry.computeVertexNormals();
+			return geometry;
+		}
+
+		var totalPlanesH = 40;
+		var totalPlanesV = 6;
+		var ODD = 1;
+		var EVEN = 2;
+
+		// material = new THREE.ParticleBasicMaterial( { color: 0x6699FF, size: 10, wireframe:true} );
+		// material = new THREE.MeshBasicMaterial( { color: 0x6699FF, wireframe:true} );
+		// material = new THREE.MeshDepthMaterial( );
+		// material = new THREE.MeshLambertMaterial( { color: 0xdddddd, shading: THREE.FlatShading } )
+		material = new THREE.MeshNormalMaterial( { color: 0x996633, shading:THREE.SmoothShading, wireframe:false} );
+		material.side = THREE.DoubleSide;
+		for(i = 0; i < totalPlanesV; i++) {
+			geometry = this.getCustomGeometry(totalPlanesH, i,i*20);
+			this.customPlane = new THREE.Mesh( geometry, material );
+			this.base.add(this.customPlane);
+		}
+
+/*
+		geometry = new THREE.Geometry();
+
+		for(i = 0; i < totalVertices; i++) {
+			geometry.vertices.push(new THREE.Vector3());
+		}
+
+		var id = 0;
+		var percentage = 0;
+		for(j = 0; j < incH; j++) {
+			for(i = 0; i < incV; i++) {
+				percentage = j/(incH-.9)*TWO_PI;
+				geometry.vertices[id].x = Math.sin(percentage)*100;
+				geometry.vertices[id].y = i*20;
+				geometry.vertices[id].z = Math.cos(percentage)*100;
+				id++;
+			}
+		}
+
+		for(j = 0; j < totalPlanesH; j++) {
+			id = j*6;
+			geometry.faces.push( new THREE.Face3( id+3, id+0, id+1 ) );
+			geometry.faces.push( new THREE.Face3( id+3, id+1, id+4 ) );
+			geometry.faces.push( new THREE.Face3( id+3, id+4, id+7 ) );
+			geometry.faces.push( new THREE.Face3( id+3, id+7, id+6 ) );
+			geometry.faces.push( new THREE.Face3( id+5, id+8, id+7 ) );
+			geometry.faces.push( new THREE.Face3( id+5, id+7, id+4 ) );
+			geometry.faces.push( new THREE.Face3( id+5, id+4, id+1 ) );
+			geometry.faces.push( new THREE.Face3( id+5, id+1, id+2 ) );
+		}
+		/*
+		for(j = 0; j < totalPlanesH; j++) {
+			id = j*6;
+			geometry.faces.push( new THREE.Face3( id+4, id+1, id+0 ) );
+			geometry.faces.push( new THREE.Face3( id+4, id+0, id+3 ) );
+			geometry.faces.push( new THREE.Face3( id+4, id+3, id+6 ) );
+			geometry.faces.push( new THREE.Face3( id+4, id+6, id+7 ) );
+
+			geometry.faces.push( new THREE.Face3( id+4, id+7, id+8 ) );
+			geometry.faces.push( new THREE.Face3( id+4, id+8, id+5 ) );
+			geometry.faces.push( new THREE.Face3( id+4, id+5, id+2 ) );
+			geometry.faces.push( new THREE.Face3( id+4, id+2, id+1 ) );
+		}
+		*/
+		/*
+		// particles = new THREE.ParticleSystem( geometry, material );
+		this.customPlane = new THREE.Mesh( geometry, material );
+		this.base.add(this.customPlane);
+		*/
 	};
+
+
+
+
+
 
 	this.createListeners = function() {
 		// this.container.addEventListener('mousedown', function(event) {
@@ -213,40 +346,53 @@ function generateTexture() {
 	this.parse = function() {
 		this.base.rotation.z += SLICER.Params.orbitSpeed;
 
-		var totalVSegments = 11;
-		var totalHSegments = 31;
-
 		var percentage;
 		var xPos, ypos, zPos;
 		id = 0;
 
 		var radius = 100 - SLICER.Params.explode;
-		for(j = 0; j < totalVSegments; j++) {
-			for(i = 0; i < totalHSegments; i++) {
-				percentage = i/(totalHSegments-1);
+		var radiusVertical = SLICER.Params.explodeVertical;
+
+		// calculates for every other totalHSegments
+		// using peaks and valley analogy
+		// peaks are the flat plateau part, valleys are the dynamic element 
+		var ifEvenVertValley;
+		var ifOddVertValley;
+		var vertCounter = 0;
+		var vertCounterIncement = 1/(this.totalHSegments-1)*300;
+		for(j = 0; j < this.totalVSegments; j++) {
+			for(i = 0; i < this.totalHSegments; i++) {
+				percentage = i/(this.totalHSegments-1);
 				xPos =  Math.cos(percentage*Math.PI*2) *100;
-				yPos = j/totalVSegments * 200 - 100;
+				yPos = vertCounter
 				zPos =  Math.sin(percentage*Math.PI*2) *100;
 
-				if(i%2==1 && j%4==0){
+				ifEvenVertValley = i%2==1 && j%4==0;
+				ifOddVertValley = i%2==0 && j%4==2;
+
+				if(ifEvenVertValley){
 					xPos =  Math.cos(percentage*Math.PI*2) *radius;
 					zPos =  Math.sin(percentage*Math.PI*2) *radius;
 				}
 
-				if(i%2==0 && j%4==2){
+
+				if(ifOddVertValley){
 					xPos =  Math.cos(percentage*Math.PI*2) *radius;
 					zPos =  Math.sin(percentage*Math.PI*2) *radius;
 				}
 
-				cylinder02.geometry.vertices[id].x = xPos;
-				cylinder02.geometry.vertices[id].y = yPos;
-				cylinder02.geometry.vertices[id].z = zPos;
+				this.cylinder.geometry.vertices[id].x = xPos;
+				this.cylinder.geometry.vertices[id].y = yPos;
+				this.cylinder.geometry.vertices[id].z = zPos;
 				id++;
 			}
+
+			if(j == 3){
+				vertCounter += vertCounterIncement;
+			} else {
+				vertCounter += vertCounterIncement;
+			}
 		}
-
-
-
 
 		this.count+=0.0125;
 
@@ -258,16 +404,16 @@ function generateTexture() {
 			particle,
 			pointsTotal = this.points.length;
 
-		var total = (cylinder02.geometry.vertices.length);
+		var total = (this.cylinder.geometry.vertices.length);
 		var inc = .5;
 		var incHalf = inc*.5;
 		for(i=0;i<total;i++){
-			cylinder02.geometry.vertices[i].x += Math.random()*inc-incHalf;
-			cylinder02.geometry.vertices[i].y += Math.random()*inc-incHalf;
-			cylinder02.geometry.vertices[i].z += Math.random()*inc-incHalf;
+			this.cylinder.geometry.vertices[i].x += Math.random()*inc-incHalf;
+			this.cylinder.geometry.vertices[i].y += Math.random()*inc-incHalf;
+			this.cylinder.geometry.vertices[i].z += Math.random()*inc-incHalf;
 		}
 */
-		cylinder02.geometry.verticesNeedUpdate = true;
+		this.cylinder.geometry.verticesNeedUpdate = true;
 		this.controls.update();
 		this.renderer.render(this.scene, this.camera);
 	};
